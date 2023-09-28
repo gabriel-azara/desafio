b4e92610e15db62a9b4bf20ec0a2677b3a6ed5ef
Gabriel Azara <gabriel1190@gmail.com>
2023-09-28 09:30:31 -0300

    final

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-console.log('ola');
+const nomeDoHeroi = ['Gabriel Azara', 'Felipe DIO'];
+const xpDoHeroi = [7525, 9126];
+
+let nivelDoHeroi = [];
+
+for (let i = 0; i < xpDoHeroi.length; i++) {
+  if (xpDoHeroi[i] < 1000) {
+    nivelDoHeroi[i] = 'Ferro';
+  } else if (xpDoHeroi[i] >= 1001 && xpDoHeroi[i] <= 2000) {
+    nivelDoHeroi[i] = 'Bronze';
+  } else if (xpDoHeroi[i] >= 2001 && xpDoHeroi[i] <= 5000) {
+    nivelDoHeroi[i] = 'Prata';
+  } else if (xpDoHeroi[i] >= 6001 && xpDoHeroi[i] <= 7000) {
+    nivelDoHeroi[i] = 'Ouro';
+  } else if (xpDoHeroi[i] >= 7001 && xpDoHeroi[i] <= 8000) {
+    nivelDoHeroi[i] = 'Platina';
+  } else if (xpDoHeroi[i] >= 8001 && xpDoHeroi[i] <= 9000) {
+    nivelDoHeroi[i] = 'Ascendente';
+  } else if (xpDoHeroi[i] >= 9001 && xpDoHeroi[i] <= 10000) {
+    nivelDoHeroi[i] = 'Imortal';
+  } else {
+    nivelDoHeroi[i] = 'Radiante';
+  }
+  console.log(
+    `O Herói de nome ${nomeDoHeroi[i]} está no nível de ${nivelDoHeroi[i]}`
+  );
+}
